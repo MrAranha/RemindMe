478d4497903f59e7c7d35a7c1b8cfc2579bfe2e3
MrAranha <aranha.breno53@gmail.com>
2023-06-21 22:13:17 -0300

    Properly read already createad files

--- src/TerminalFeatures/ListAndSelectFiles.java
+++ src/TerminalFeatures/ListAndSelectFiles.java
@@ -1,23 +1,58 @@
 package TerminalFeatures;
 
+import DTO.FilesDTO;
+
 import java.io.File;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.nio.file.Paths;
 
 public class ListAndSelectFiles {
-    public static String[] ListFiles(){
-        System.out.println("");
+    private static boolean CheckIfSelectionIsAFile(String Selection, ArrayList<FilesDTO> FilesDTOList)
+    {
+        for(int i = 0; i < FilesDTOList.size(); i++){
+            String PathNum = String.valueOf(FilesDTOList.get(i).getFileNumberMapping());
+            if(PathNum.equals(Selection)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    private static ArrayList<FilesDTO> ListFiles(){
         File folder = new File(System.getenv("APPDATA") + "\\" + "Notes");
-        String[] pathnames;
-        pathnames = folder.list();
-        if(pathnames == null){
+        String[] RawPathnames;
+        ArrayList<FilesDTO> FilesDTOList = new ArrayList<FilesDTO>();
+
+        RawPathnames = folder.list();
+        if(RawPathnames == null){
             System.out.println(ConsoleColors.getWhite() + "Não há nenhuma anotação em seu computador!" + ConsoleColors.getReset());
             return null;
         }
-
-        for(int i = 0; i < pathnames.length; i++) {
-            System.out.println(ConsoleColors.getYellow() + pathnames[i] + ConsoleColors.getReset());
+        System.out.println(ConsoleColors.getWhite() + "===========================" + ConsoleColors.getReset());
+        for(int i = 0; i < RawPathnames.length; i++) {
+            FilesDTO File = new FilesDTO();
+            Path name = Paths.get(RawPathnames[i]);
+            File.setFileName(name.getFileName().toString());
+            File.setFilePath(RawPathnames[i]);
+            File.setFileNumberMapping(i);
+            FilesDTOList.add(File);
+            System.out.println(ConsoleColors.getYellow() + RawPathnames[i] + ConsoleColors.getReset());
         }
-        return pathnames;
+        return FilesDTOList;
     }
-
-
+    public static void AnnotationSelect() {
+        Scanner scanner = new Scanner(System.in);
+        String Selected = "";
+        ArrayList<FilesDTO> FilesDTOList = ListFiles();
+        if(FilesDTOList == null)
+            return;
+        System.out.println(ConsoleColors.getRed() + "Por favor selecione uma anotação!" + ConsoleColors.getReset());
+        do {
+            Selected = scanner.nextLine().trim();
+        } while(!CheckIfSelectionIsAFile(Selected, FilesDTOList));
+        System.out.println(ConsoleColors.getGreen() + "Sucesso!" + ConsoleColors.getReset());
+    }
 }
